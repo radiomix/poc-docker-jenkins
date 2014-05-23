8d0be31e5e7b59c96b0b499390a787e13c0196f9
Michael Klöckner <mkl@im7.de>
2014-05-23 12:18:46 +0000

    added dockerize to base ami and test.json packer test file

--- packer/dockerize.sh
+++ packer/dockerize.sh
@@ -1,3 +1,5 @@
-# Load dockerize automatically by adding 
-eval "$(/home/ubuntu/docker/dockerize/bin/dockerize init -)" 
+#!/bin/bash
+echo '# Load dockerize automatically ' >> /home/ubuntu/.bashrc 
+echo 'eval "$(/home/ubuntu/docker/dockerize/bin/dockerize init -)" '  >> /home/ubuntu/.bashrc
+echo " " >> /home/ubuntu/.bashrc
 
