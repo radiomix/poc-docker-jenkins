ddf8fc6f2cdf34fe257fce1a391c2cd6c608ac54
Michael Klöckner <mkl@im7.de>
2014-06-03 18:34:33 +0000

    apache basic auth FIXME
    Former-commit-id: d206cd82653d77321cb11a12e84137128b2c5635

--- container_builds/registry/start_script.sh
+++ container_builds/registry/start_script.sh
@@ -4,7 +4,15 @@
 # This script contains start commands for the docker registry
 #
 
+# we need apache for basic authentication
+# and proxy to localhost:5000
+service apache2 stop
+service apache2 start
 
+
+# the binary docker-registry executable
+# maybe we could start the unicorn app like
+# in https://github.com/dotcloud/docker-registry
+# gunicorn -k gevent --max-requests 100 --graceful-timeout 3600 -t 3600 -b localhost:5000 -w 8 docker_registry.wsgi:application
 docker-registry 
-exit
 
