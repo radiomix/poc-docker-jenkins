a5aeb405912191c96ae158123730a30fba5093e2
Michael Klöckner <mkl@im7.de>
2014-06-07 18:21:07 +0000

    renamed run.sh to test.sh

---
+++ container_builds/nginx/test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+CONT_NAME="nginx_kweb"
+IMG_NAME="nginx/kweb"
+
+# delete old container
+sudo docker kill $CONT_NAME #&>/dev/null
+sudo docker rm $CONT_NAME #&>/dev/null
+
+# build the container and run it
+sudo docker build --rm --no-cache -t $IMG_NAME .
+NGINX=$(sudo docker run -d -p 8001:8001 --name $CONT_NAME $IMG_NAME)
+
+echo Running $CONT_NAME PID:$NGINX 
+echo "To kill it type :> sudo docker kill $CONT_NAME; sudo docker rm $CONT_NAME"
+
