8767e991095d67c036ed0d8e111220320f84f046
Michael Klöckner <mkl@im7.de>
2014-06-16 18:56:01 +0000

    REG_SHIPYARD
    Former-commit-id: b7bfa2c9746b2c0582cadaca0fdbed3ffc557a74

--- container_builds/nginx/test.sh
+++ container_builds/nginx/test.sh
@@ -7,10 +7,33 @@ IMG_NAME="nginx/kweb"
 sudo docker kill $CONT_NAME #&>/dev/null
 sudo docker rm $CONT_NAME #&>/dev/null
 
+# test for input parameter
+case "$1" in
+# ----------------------------------------------------------- #
+ -b|--build)
+        echo " Building nginx "
 # build the container and run it
 sudo docker build --rm --no-cache -t $IMG_NAME .
+        ;;
+# ----------------------------------------------------------- #
+ -d|--daemon)
+        echo " Running nginx as a daemon"
 NGINX=$(sudo docker run -d -p 8001:8001 --name $CONT_NAME $IMG_NAME)
-
-echo Running $CONT_NAME PID:$NGINX 
-echo "To kill it type :> sudo docker kill $CONT_NAME; sudo docker rm $CONT_NAME"
-
+        ;;
+# ----------------------------------------------------------- #
+ -i|--interactive)
+        echo " Running nginx interactively "
+sudo docker run -i -t -p 8001:8001 --name $CONT_NAME $IMG_NAME /bin/bash
+        ;;
+# ----------------------------------------------------------- #
+ -h|--help|*)
+  echo "
+ usage: 
+test.sh -i --interactive run container $IMG_NAME interactively
+test.sh -d --daemon 	 run container $IMG_NAME as a daemon
+test.sh -b --build	 build the container 
+test.sh -h --help        this message
+      "
+  exit
+        ;;
+esac
