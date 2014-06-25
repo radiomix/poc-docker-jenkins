aae057c457063770e3367ef4c9a1e2a7f7ae36e5
Michael Klöckner <mkl@im7.de>
2014-06-25 10:22:33 +0000

    test to run configured or base registry
    Former-commit-id: 2ff1870c4ac0c9af1a1a65695822ee11568adfe9

--- container_builds/registry/test.sh
+++ container_builds/registry/test.sh
@@ -1,14 +1,38 @@
 #!/bin/bash
 
-
-# we just configure the base registry
-./build.sh -c 
-
 # this is the name of the running container
 CONT_NAME=my_registry
 
+
+
+case "$1" in
+# ----------------------------------------------------------- #
+ -b|--base)
+TAG_NAME=base
+
+        ;;
+# ----------------------------------------------------------- #
+ -c|--configured)
+TAG_NAME=run
+
+        ;;
+# ----------------------------------------------------------- #
+ -h|--help|*)
+  echo "
+ usage: 
+test.sh -b --base      run the base container 
+test.sh -c --config    run the configured continer
+test.sh -h --help      this message
+      "
+exit
+        ;;
+esac
+# ----------------------------------------------------------- #
+
+#cleaning up old container
 docker kill $CONT_NAME
 docker rm $CONT_NAME
 
 
-docker run -i -t -v /registry-storage:/tmp/ -p 5000:5000 --name $CONT_NAME my-registry:run docker-registry
+echo docker run -i -t -v /registry-storage:/tmp/ -p 5000:5000 --name $CONT_NAME my-registry:$TAG_NAME docker-registry
+docker run -i -t -v /registry-storage:/tmp/ -p 5000:5000 --name $CONT_NAME my-registry:$TAG_NAME docker-registry
