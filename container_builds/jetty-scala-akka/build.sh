ce3f8ae11dde198ae0e2701aec12cce87c1e1aff
Michael Klöckner <mkl@im7.de>
2014-07-07 18:04:33 +0000

    building akka sample scala web app JAR file only
    Former-commit-id: c7d06dcf9aaa65f11c66c303d73e6fd35fefe650

--- container_builds/jetty-scala-akka/build.sh
+++ container_builds/jetty-scala-akka/build.sh
@@ -8,6 +8,7 @@ APT_INST="apt-get install -y --no-install-recommends"
 CWD=$(pwd)
 
 echo " INSTALLING UTILS "
+echo "
 sudo apt-get update
 sudo $APT_INST apt-utils
 
@@ -16,37 +17,58 @@ sudo $APT_INST zip
 sudo $APT_INST git
 sudo $APT_INST unzip
 sudo $APT_INST openjdk-7-jdk
-
+FIXME FIXME in $0 FIXME FIXME FIXME in $0 
+"
+#####################################
 echo " INSTALLING SCALA and SBT "
 sudo $APT_INST scala
 wget -O /tmp/sbt-0.13.5.deb http://dl.bintray.com/sbt/debian/sbt-0.13.5.deb
 sudo dpkg -i /tmp/sbt-0.13.5.deb
 
+#####################################
+echo " INSTALLING TYPESAFE AKKA "
+sudo mkdir /opt/typesafe &> /dev/null
+sudo chown -R ubuntu /opt/typesafe &>/dev/null 
+if [ -s /tmp/typesafe-activator-1.2.3.zip ]
+ then 
+ echo " Skipping download activator file" 
+ else 
+   wget -O /tmp/typesafe-activator-1.2.3.zip http://downloads.typesafe.com/typesafe-activator/1.2.3/typesafe-activator-1.2.3.zip
+ echo "Typesafe Activator downloaded!"
+fi
+
+unzip -o -qq -d /opt/typesafe  /tmp/typesafe-activator-1.2.3.zip 
+export TYPESAFE="/opt/typesafe/activator-1.2.3/"
 
 #####################################
-echo " INSTALLING SCALA/LIFT EXAMPLE "
-export WEBAPP_HELLO="/opt/hello/"
-export WEBAPP_HELLO_JAR=$WEBAPP_HELLO/target/scala-2.10/
-#wget -O /tmp/master.zip https://github.com/Lift/Lift_26_sbt/archive/master.zip 
-sudo mkdir $WEBAPP_HELLO &> /dev/null
-sudo chown -R ubuntu $WEBAPP_HELLO
+export WEBAPP_AKKA="activator-akka-tracing"		#this is the sample app
+echo " INSTALLING SCALA/LIFT EXAMPLE $WEBAPP_AKKA"
+export WEBAPP_AKKA_JAR=$WEBAPP_AKKA/target/scala-2.11/
+cd $TYPESAFE
+rm -rf $WEBAPP_AKKA &>/dev/null
+# create the web app with akka:> ./activator new PROJECTNAME activator-akka-tracing
+./activator new $WEBAPP_AKKA $WEBAPP_AKKA             
 
-cd $WEBAPP_HELLO/
-echo 'object Hi { def main(args: Array[String]) = println("Hi!") }' > hw.scala
-# create a rudimentary build.sbt file
-touch $WEBAPP_HELLO/build.sbt 
-cat << EOF > $WEBAPP_HELLO/build.sbt 
-name := "hello"
 
-version := "1.0"
+#
+#prepare to package the war file
 
-scalaVersion := "2.10.3"
-
-EOF
+echo " Preparing to build a WAR file "
+cd $TYPESAFE$WEBAPP_AKKA
 
 sbt compile
 sbt package
 
+
+# copy jar file into the jetty directory
+export WEBAPPS=/opt/jetty/webapps
+sudo mkdir -p $ WEBAPPS &>/dev/null
+cp $TYPESAFE$WEBAPP_AKKA_JAR/*jar $WEBAPPS
+sudo chown -R ubuntu $ WEBAPPS
+ls  $WEBAPPS
+
+
+#####################################
 cd $CWD
 echo "BUILDING NEW JETTY CONTAINER in $(pwd)"
 JETTY_NAME="jetty/jetty-demo:example"
@@ -54,17 +76,13 @@ CONTAINER_NAME="jetty_scala"
 #sudo docker rmi $JETTY_NAME  &> /dev/null           # remove old build artefact
 sudo docker build --rm  -t $JETTY_NAME .             # rebuild new image
 
-export WEBAPPS=/opt/jetty/webapps
-sudo mkdir -p $ WEBAPPS &>/dev/null
-sudo chown -R ubuntu $ WEBAPPS
 
-ls  $WEBAPP_HELLO_JAR
 
 #kill/remove old container and run the new one
 sudo docker kill $CONTAINER_NAME &>/dev/null  
 sudo docker rm $CONTAINER_NAME &>/dev/null
 JETTY=$(docker run -d --name=$CONTAINER_NAME -p 80:8080 -v $WEBAPPS:/opt/jetty/webapps jetty/jetty-demo:example)
-cp $WEBAPP_HELLO_JAR/*jar $WEBAPPS
+
 
 echo "
 #############################
