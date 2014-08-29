6eb54a1439385d8bd471100172801a0255fc0d30
Michael Klöckner <mkl@im7.de>
2014-08-29 17:10:25 +0200

    type fix

--- container_builds/registry/README.md
+++ container_builds/registry/README.md
@@ -2,10 +2,11 @@
 The registry is based on [registry github](http://github.com/docker/docker-registry/).
 ### Configuration
 We just provide a local [`confgi.yml`](config.yml) for commodity reasons. This file can be tweaked.
+I is added to the container at runtime.
 
 ### Start script
 We use script [`start_registry.sh`](start_registry.sh) to set some environment variables 
-(espacially AWS options) and start the container.
+(espacially AWS options), add the config file and start the container.
 
 ### Notes
 The registry wont accept env variable `AWS_REGION` in start up command as stated by [issue 400](https://github.com/docker/docker-registry/issues/400).
--- container_builds/registry/start_registry.sh
+++ container_builds/registry/start_registry.sh
@@ -11,13 +11,14 @@
 
 # Each config option is either set inside the registry config file
 # or as a variable to registry start call.
-CWD=$(pwd)
-CONFIG_FILE="$CWD/config.yml"
+
 
 # The env variable  DOCKER_REGISTRY_CONFIG  points to the config file 
-# /docker-registry/config/config_sample.yml inside the container at 
-# We this inject our local file .config.yml into the container.
+# /docker-registry/config/config_sample.yml inside the container.
+# We this add our local file .config.yml into the container.
 # But we have to give the absolute path
+CWD=$(pwd)
+CONFIG_FILE="$CWD/config.yml"
 REGISTRY_CONFIG_OPTION=" -v $CONFIG_FILE:/docker-registry/config/config_sample.yml " 
 
 
@@ -28,13 +29,13 @@ REGISTRY_PORT_OPTION=" -p $REGISTRY_HOST_PORT:$REGISTRY_CONTAINER_PORT "
 
 
 # AWS S3 options
-AWS_REGION="us-west-2"
 AWS_BUCKET="im7-registry "
 STORAGE_PATH="/registry"
 SEARCH_BACKEND="sqlalchemy"
 # We expect AWS_KEY and AWS_SECRET to be env variables 
 AWS_KEY=$(echo $AWS_KEY)
 AWS_SECRET=$(echo $AWS_SECRET)
+# AWS_REGION="us-west-2" !!! This env  variable is not accepted by the registry and prevents it from starting!
 
 AWS_OPTION=" -e AWS_BUCKET=$AWS_BUCKET -e STORAGE_PATH=$STORAGE_PATH  -e AWS_KEY=$AWS_KEY -e AWS_SECRET=$AWS_SECRET -e SEARCH_BACKEND=$SEARCH_BACKEND "
 
@@ -56,7 +57,9 @@ docker kill $CONTAINER_NAME
 docker rm   $CONTAINER_NAME
 
 # start a new container
-#echo docker run $REGISTRY_PORT_OPTION --name $CONTAINER_NAME $REGISTRY_SETTINGS_FLAVOR $AWS_OPTION $REGISTRY_CONFIG_OPTION $REGISTRY_IMAGE
+echo "THIS IS OUR START COMMAND "
+echo docker run $REGISTRY_PORT_OPTION --name $CONTAINER_NAME $REGISTRY_SETTINGS_FLAVOR $AWS_OPTION $REGISTRY_CONFIG_OPTION $REGISTRY_IMAGE
+echo 
 docker run $REGISTRY_PORT_OPTION --name $CONTAINER_NAME $REGISTRY_SETTINGS_FLAVOR $AWS_OPTION $REGISTRY_CONFIG_OPTION $REGISTRY_IMAGE
 
 
